5226fa59420b16d197d859d420c196dd81e7b821
steliosk9 <steliosk9@gmail.com>
2019-08-18 11:29:39 -0500

    refactor the handlebar files

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
-
-
 var mongoose = require("mongoose");
 var logger = require("morgan");
 var exphbs = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var path = require('path');
+
+var Comment = require("../models/Article.js")
+var Note = require("../models/Note.js")
 
 
 var express = require("express");
@@ -46,25 +48,34 @@ db.once('open', function() {
 
 // // Routes
 
-// // A GET route for scraping the echoJS website
-// app.get("/scrape", function(req, res) {
-//   // First, we grab the body of the html with axios
-//   axios.get("http://www.echojs.com/").then(function(response) {
-//     // Then, we load that into cheerio and save it to $ for a shorthand selector
-//     var $ = cheerio.load(response.data);
+app.get("/", function(req, res){
+  res.redirect("/articles");
+});
 
-//     // Now, we grab every h2 within an article tag, and do the following:
-//     $("article h2").each(function(i, element) {
-//       // Save an empty result object
-//       var result = {};
+// app.get("/", function(req, res){
+//   res.redirect("/articles");
+// });
 
-//       // Add the text and href of every link, and save them as properties of the result object
-//       result.title = $(this)
-//         .children("a")
-//         .text();
-//       result.link = $(this)
-//         .children("a")
-//         .attr("href");
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.ablogtowatch.com/page/2/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object   ============
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
 
 //       // Create a new Article using the `result` object built from scraping
 //       db.Article.create(result)
